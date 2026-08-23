e3076b44311077d2bf6d50380a3dcee27d7e9e93
RajaYoken7 <rajayoken072@gmail.com>
2026-08-23 20:39:03 +0530

    Initial SIH 2026 project setup

---
+++ Mat-Lab/DR-project/scripts/day1_basics.m
@@ -0,0 +1,11 @@
+% Clear the Command Window
+clc; 
+
+% Remove all variables from the Workspace
+clear;
+
+% Close all open figure and image windows
+close all;
+
+% Display a welcome message in the Command Window
+disp("Hello! Starting my DR AI Project");
