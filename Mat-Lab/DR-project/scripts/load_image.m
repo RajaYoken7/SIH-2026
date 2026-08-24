cda415f427e7ecabd4403bcf6550fbb09f5521d4
RajaYoken7 <rajayoken072@gmail.com>
2026-08-24 19:30:56 +0530

    basic mat lab undersatanding

---
+++ Mat-Lab/DR-project/scripts/load_image.m
@@ -0,0 +1,11 @@
+clc;
+clear;
+close all;
+
+% Load an image
+img = imread('C:\Me\coding\SIH-2026\Mat-Lab\DR-project\data\test.jpeg');
+
+% Display the image
+figure;
+imshow(img);
+title('My First Image in MATLAB');
